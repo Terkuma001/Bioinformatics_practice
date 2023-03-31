f29a5113bf1ec98ea9c85a8e8de4a9db4b85a541
Terkuma001 <saaondoterkuma@gmail.com>
2023-03-31 15:39:48 +0100

    Bases count

---
+++ main.py
@@ -0,0 +1,13 @@
+dna_sequence = input("Enter DNA sequence: ")
+
+num_a = dna_sequence.count("A")
+num_c = dna_sequence.count("C")
+num_g = dna_sequence.count("G")
+num_t = dna_sequence.count("T")
+num_u = dna_sequence.count("U")
+
+print("Number of A's:", num_a)
+print("Number of C's:", num_c)
+print("Number of G's:", num_g)
+print("Number of T's:", num_t)
+print("Number of U's:", num_u)
